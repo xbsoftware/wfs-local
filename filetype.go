5e2cde57cbd5492177061b43906ffd6c51ba17fb
Nastya <akovalenko@xbsoftware.com>
2020-02-03 19:31:36 +0300

    [fix] accounting for file extensions in upper case

--- filetype.go
+++ filetype.go
@@ -3,6 +3,7 @@ package wfs
 import (
 	"os"
 	"path/filepath"
+	"strings"
 )
 
 var types map[string]string
@@ -55,7 +56,7 @@ func getType(info os.FileInfo) string {
 		return "file"
 	}
 
-	ftype, ok := types[ext[1:]]
+	ftype, ok := types[strings.ToLower(ext[1:])]
 	if !ok {
 		return "file"
 	}
